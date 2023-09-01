50b0b79afc51a0987c9ea739694826809cbf4b1d
gabrielreiss <gabrielreissdecastro@gmail.com>
2023-08-31 21:53:04 -0300

    Aula 1

---
+++ aula1/ex/index.js
@@ -0,0 +1,4 @@
+const nome = "Gabriel";
+var hora = 22;
+
+console.log(`Meu nome é ${nome}. Estou aprendendo JavaScript às ${hora} da noite`);
---
+++ aula1/index.js
@@ -0,0 +1,5 @@
+console.log("Gabriel 'Reiss'");
+console.log('Gabriel "Reiss"');
+console.log(`'Gabriel' "Reiss"`);
+
+console.log(35, 15.85, "Gabriel Reiss");
