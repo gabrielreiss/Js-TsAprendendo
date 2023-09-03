15508514b1caaed47b18801a8b5afa2c9d5058b2
gabrielreiss <gabrielreissdecastro@gmail.com>
2023-09-03 18:17:20 -0300

    aula sobre numbers

---
+++ aula10/index.js
@@ -0,0 +1,9 @@
+let num1 = 10.568452156;
+
+console.log(num1.toFixed(2)); //arredondar para duas casas depois da virgula
+
+console.log(Number.isInteger(num1));
+
+console.log(Number.isNaN(num1));
+
+console.log(parseFloat(num1.toFixed(2))); //corrigir cagadas do javascript
---
+++ aula11/index.js
@@ -0,0 +1,6 @@
+let num1 = 9.56845;
+//let num2 = Math.floor(num1);
+//let num2 = Math.ceil(num1);
+let num2 = Math.round(num1);
+
+console.log(num2);
