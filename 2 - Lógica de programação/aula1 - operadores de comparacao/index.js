3fc623984288ef66584bc9677f090dcefc24922d
gabrielreiss <gabrielreissdecastro@gmail.com>
2023-09-09 11:16:07 -0300

    feature: seção 2

---
+++ 2 - Lógica de programação/aula1 - operadores de comparacao/index.js
@@ -0,0 +1,17 @@
+/*
+== igualdade (valor)
+=== igualdade estrita (valor e tipo)
+!= diferente (valor)
+!== diferente estrito (valor e tipo)
+*/
+
+// console.log(10 > 5);
+// console.log(10 >= 11);
+// console.log(10 < 11);
+// console.log(12 <= 11);
+
+const num1 = 10
+const num2 = '10'
+// const comp = num1 == num2
+const comp = num1 === num2
+console.log(comp)
---
+++ 2 - Lógica de programação/aula2 - Operadores lógicos/index.js
@@ -0,0 +1,9 @@
+/*
+Operadores lógicos
+&& -> AND -> Todas as expressões precisam ser verdadeiras para retornar true
+|| -> OR  -> 
+! -> NOT
+*/
+
+console.log(true && true); //and
+console.log(true || false); //or
